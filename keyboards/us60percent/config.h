9db78d6490d19b7f3c5d3eb05fcf3306311908ea
yuracream <96818771+yuracream@users.noreply.github.com>
2025-10-25 19:07:01 +0900

    Update config.h

--- keyboards/us60percent/config.h
+++ keyboards/us60percent/config.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "config_common.h"
+
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 14
 #define DIODE_DIRECTION COL2ROW
+
+/* あなたの基板に合わせて。以前の値が 1MB だったので同じに */
 #define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
