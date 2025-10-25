2dd539e9e6132521e266244ce0a92718a857c983
yuracream <96818771+yuracream@users.noreply.github.com>
2025-10-25 18:50:35 +0900

    Update config.h

--- keyboards/us60percent/config.h
+++ keyboards/us60percent/config.h
@@ -1,9 +1,6 @@
 #pragma once
 #include "config_common.h"
-
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 14
 #define DIODE_DIRECTION COL2ROW
-
-/* RP2040 flash size (あなたの基板仕様に合わせる) */
-#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)  /* ← 以前の値が1MBだったので同じに */
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
