e1738bda7890a715309c44edf06763152b4f335e
yuracream <96818771+yuracream@users.noreply.github.com>
2025-10-25 18:50:20 +0900

    Update rules.mk

--- keyboards/us60percent/rules.mk
+++ keyboards/us60percent/rules.mk
@@ -1,15 +1,6 @@
-# --- us60percent (RP2040) base rules ---
 MCU = RP2040
 BOOTLOADER = rp2040
-
-# 最適化（任意だが推奨）
 LTO_ENABLE = yes
-
-# VIA/Vial 対応（Vialを使うなら両方ON）
+# （あれば）BOARD = GENERIC_RP2040
 VIA_ENABLE  = yes
 VIAL_ENABLE = yes
-
-# そのほか必要に応じて
-# CONSOLE_ENABLE = no
-# MOUSEKEY_ENABLE = yes
-# EXTRAKKEY_ENABLE = yes
